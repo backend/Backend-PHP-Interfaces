c02438ff74d7d0a4f77716bbef804d997ed88be1
Jurgens du Toit <jrgns@jrgns.net>
2012-06-13 05:09:12 +0200

    Removed invalid Core NS, added ConfigInterface

--- ApplicationInterface.php
+++ ApplicationInterface.php
@@ -1,37 +1,35 @@
 <?php
 /**
- * File defining ApplicationInterface.
+ * File defining Backend\Interfaces\ApplicationInterface.
  *
  * PHP Version 5.3
  *
- * @category   Backend
- * @package    Core
- * @subpackage Interfaces
- * @author     J Jurgens du Toit <jrgns@backend-php.net>
- * @copyright  2011 - 2012 Jade IT (cc)
- * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @link       http://backend-php.net
+ * @category  Backend
+ * @package   Interfaces
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
  */
-namespace Backend\Core\Interfaces;
+namespace Backend\Interfaces;
 /**
  * The Application class wraps the execution process.
  *
- * @category   Backend
- * @package    Core
- * @subpackage Interfaces
- * @author     J Jurgens du Toit <jrgns@backend-php.net>
- * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @link       http://backend-php.net
+ * @category Backend
+ * @package  Interfaces
+ * @author   J Jurgens du Toit <jrgns@backend-php.net>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://backend-php.net
  */
-interface Application
+interface ApplicationInterface
 {
     /**
      * Main function for the application
      *
-     * @param \Backend\Core\Request $request The request the application should
-     * handle
+     * @param \Backend\Interfaces\RequestInterface $request The request the
+     * application should handle
      *
      * @return mixed The result of the call
      */
-    public function main(\Backend\Core\Interfaces\RequestInterface $request);
+    public function main(\Backend\Interfaces\RequestInterface $request = null);
 }
--- FormatterInterface.php
+++ FormatterInterface.php
@@ -1,27 +1,25 @@
 <?php
 /**
- * File defining FormatterInterface.
+ * File defining Backend\Interfaces\FormatterInterface.
  *
  * PHP Version 5.3
  *
- * @category   Backend
- * @package    Core
- * @subpackage Interfaces
- * @author     J Jurgens du Toit <jrgns@backend-php.net>
- * @copyright  2011 - 2012 Jade IT (cc)
- * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @link       http://backend-php.net
+ * @category  Backend
+ * @package   Interfaces
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
  */
-namespace Backend\Core;
+namespace Backend\Interfaces;
 /**
  * Transform results into the specified format.
  *
- * @category   Backend
- * @package    Core
- * @subpackage Interfaces
- * @author     J Jurgens du Toit <jrgns@backend-php.net>
- * @license    http://www.opensource.org/licenses/mit-license.php MIT License
- * @link       http://backend-php.net
+ * @category Backend
+ * @package  Interfaces
+ * @author   J Jurgens du Toit <jrgns@backend-php.net>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://backend-php.net
  */
 interface FormatterInterface
 {
@@ -30,7 +28,7 @@ interface FormatterInterface
      *
      * @param mixed $result The result to transform.
      *
-     * @return \Backend\Core\Interfaces\ResponseInterface;
+     * @return \Backend\Interfaces\ResponseInterface;
      */
     public function transform($result);
 }
