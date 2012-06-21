f8e4cf94c4761b1cce609084fd282eac18b57253
Jurgens du Toit <jrgns@jrgns.net>
2012-06-21 08:15:28 +0200

    Added CallbackFactoryInterface

---
+++ CallbackFactoryInterface.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * File defining Backend\Interfaces\CallbackFactoryInterface.
+ *
+ * PHP Version 5.3
+ *
+ * @category  Backend
+ * @package   Interfaces
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
+ */
+namespace Backend\Interfaces;
+/**
+ * Class to create callbacks from strings and arrays.
+ *
+ * @category Backend
+ * @package  Interfaces
+ * @author   J Jurgens du Toit <jrgns@backend-php.net>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://backend-php.net
+ */
+interface CallbackFactoryInterface
+{
+    /**
+     * Convert a string to a callback.
+     *
+     * @param string $string    The string representation of the callback.
+     * @param array  $arguments The arguments for the callback.
+     *
+     * @return CallbackInterface
+     */
+    public function fromString($string, array $arguments = array());
+}
