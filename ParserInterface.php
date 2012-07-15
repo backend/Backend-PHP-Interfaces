c1447cf5d5d6e012462f556cf5211e8c61941432
Jurgens du Toit <jrgns@jrgns.net>
2012-07-15 23:19:52 +0200

    WIP Trying to get DIC working

---
+++ ParserInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * File defining Backend\Interfaces\ParserInterface.
+ *
+ * PHP Version 5.3
+ *
+ * @category  Backend
+ * @package   Interfaces
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
+ */
+namespace Backend\Interfaces;
+/**
+ * A dependency injection container that manages dependencies.
+ *
+ * @category Backend
+ * @package  Interfaces
+ * @author   J Jurgens du Toit <jrgns@backend-php.net>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://backend-php.net
+ */
+interface ParserInterface
+{
+
+}
