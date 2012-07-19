558faab410d9fefa5f97e56c5cb6ea6d8893803a
Jurgens du Toit <jrgns@jrgns.net>
2012-07-19 09:25:26 +0200

    Added constructor, get and removeDefinition to DIC Interface

--- DependencyInjectorContainerInterface.php
+++ DependencyInjectorContainerInterface.php
@@ -23,5 +23,33 @@ namespace Backend\Interfaces;
  */
 interface DependencyInjectorContainerInterface
 {
+    /**
+     * The object constructor.
+     *
+     * @param Backend\Interfaces\ConfigInterface $config The configuration file
+     * to check for service and parameter definitions.
+     */
+    public function __construct(ConfigInterface $config);
+
+    /**
+     * Get the Implementation of the specified Component.
+     *
+     * @param string $id The Component identifier.
+     * @param integer $invalidBehaviour The behavior when the service does not exist.
+     *
+     * @return object
+     * @throws \Backend\Core\Exception
+     */
+    public function get($id,
+        $invalidBehavior = ContainerInterface::IGNORE_ON_INVALID_REFERENCE);
+
+    /**
+     * Remove the Implementation of the specified Component.
+     *
+     * @param string $component The name of the Component to remove.
+     *
+     * @return void
+     */
+    public function removeDefinition($id);
 
 }
