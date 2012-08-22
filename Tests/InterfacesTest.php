37a6e3d3104b8e559d14206580b6a857e2d471a3
Jurgens du Toit <jurgens@everlytic.com>
2012-08-22 07:56:09 +0200

    Added Tests

---
+++ Tests/InterfacesTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * File defining InterfacesTest
+ *
+ * PHP Version 5.3
+ *
+ * @category  Backend
+ * @package   InterfacesTests
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
+ */
+namespace Backend\Interfaces\Tests;
+use Backend\Interfaces\ApplicationInterface;
+/**
+ * Class to test all the defined interfaces
+ *
+ * @category Backend
+ * @package  InterfacesTests
+ * @author   J Jurgens du Toit <jrgns@backend-php.net>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://backend-php.net
+ */
+class InterfacesTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Data provider for testInterfaces. Return all the interfaces.
+	 * 
+	 * @return array
+	 */
+	public function dataInterfaces()
+	{
+		$files = glob(__DIR__ . '/../*Interface.php');
+		$files = array_map(array($this, 'getArguments'), $files);
+		return $files;
+	}
+
+	protected function getArguments($file)
+	{
+		$file = basename($file);
+		if (substr($file, -4) === '.php') {
+			$file = substr($file, 0, strlen($file) - 4);
+		}
+		return array($file);
+	}
+
+	/**
+	 * Compile all the interfaces.
+	 * 
+	 * @dataProvider dataInterfaces
+	 */
+	public function testInterfaces($interface)
+	{
+		$interface = '\Backend\Interfaces\\' . $interface;
+		$object = $this->getMockForAbstractClass($interface);
+		$this->assertInstanceOf($interface, $object);
+	}
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The bootstrapping script
+ *
+ * PHP Version 5.3
+ *
+ * @category  Backend
+ * @package   Interfaces
+ * @author    J Jurgens du Toit <jrgns@backend-php.net>
+ * @copyright 2011 - 2012 Jade IT (cc)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link      http://backend-php.net
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * Register and  load available autoloaders
+ */
+spl_autoload_register(function ($class) {
+    if (substr($class, 0, strlen('Backend\Interfaces\\')) === 'Backend\Interfaces\\') {
+        $class = substr($class, strlen('Backend\Interfaces\\'));
+    }
+    if (file_exists($file = __DIR__ . '/../' . $class . '.php')) {
+        require_once $file;
+    }
+});
