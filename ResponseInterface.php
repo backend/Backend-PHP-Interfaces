57770323ae8e25170c6eb237d674a1909fb897a0
Jurgens du Toit <jrgns@jrgns.net>
2012-09-05 08:51:08 +0200

    Extended the Response Interface

--- ResponseInterface.php
+++ ResponseInterface.php
@@ -24,6 +24,63 @@ namespace Backend\Interfaces;
 interface ResponseInterface
 {
     /**
+     * Return the current status code for the Response
+     *
+     * @return int The status code
+     */
+    public function getStatusCode();
+
+    /**
+     * Set the status code for the Response
+     *
+     * @param int $code The new status code
+     *
+     * @return Response The current object
+     */
+    public function setStatusCode($code);
+
+    /**
+     * Return the Response's body
+     *
+     * @return mixed The Response's body
+     */
+    public function getBody();
+
+    /**
+     * Set the body for the Response
+     *
+     * @param mixed $body The new body
+     *
+     * @return Response The current object
+     */
+    public function setBody($body);
+
+    /**
+     * Add a header to the Response.
+     *
+     * @param string $content The content of the header
+     *
+     * @return Response The current object
+     */
+    public function addHeader($content);
+
+    /**
+     * Return the Response's headers
+     *
+     * @return array An array containing the Response's headers
+     */
+    public function getHeaders();
+
+    /**
+     * Set the headers for the Response
+     *
+     * @param array $headers The new headers
+     *
+     * @return Response The current object
+     */
+    public function setHeaders(array $headers);
+
+    /**
      * Output the response to the client.
      *
      * @return null
