4f5ade8e59321b780f34c32352fc890dd0098baf
Jurgens du Toit <jrgns@jrgns.net>
2012-09-14 08:24:07 +0200

    Added more BindingFactory methods

--- BindingFactoryInterface.php
+++ BindingFactoryInterface.php
@@ -24,6 +24,14 @@ namespace Backend\Interfaces;
 interface BindingFactoryInterface
 {
     /**
+     * The class constructor.
+     *
+     * @param Backend\Interfaces\ConfigInterface $bindings    The bindings config.
+     * @param Backend\Interfaces\ConfigInterface $connections The connections config.
+     */
+    public function __construct(ConfigInterface $bindings, ConfigInterface $connections);
+
+    /**
      * Convert a string to a callback.
      *
      * @param string $modelName The name of the model to get the Binding for.
@@ -31,4 +39,68 @@ interface BindingFactoryInterface
      * @return CallbackInterface
      */
     public function build($modelName);
+
+    /**
+     * Get the Bindings Configuration.
+     *
+     * @return Backend\Interfaces\ConfigInterface
+     */
+    public function getBindings();
+
+    /**
+     * Set the Bindings Configuration.
+     *
+     * @param Backend\Interfaces\ConfigInterface
+     *
+     * @return Backend\Base\Utilities\BindingFactory
+     */
+    public function setBindings(ConfigInterface $bindings);
+
+    /**
+     * Get the specified Binding.
+     *
+     * @return array|null The Binding or null if it doesn't exist.
+     */
+    public function getBinding($name);
+
+    /**
+     * Set the values of the specified Binding.
+     *
+     * @param array $binding The Binding described as an array.
+     *
+     * @return Backend\Base\Utilities\BindingFactory
+     */
+    public function setBinding($name, array $binding);
+
+    /**
+     * Get the Connections Configuration.
+     *
+     * @return Backend\Interfaces\ConfigInterface
+     */
+    public function getConnections();
+
+    /**
+     * Set the Connections Configuration.
+     *
+     * @param Backend\Interfaces\ConfigInterface
+     *
+     * @return Backend\Base\Utilities\BindingFactory
+     */
+    public function setConnections(ConfigInterface $connections);
+
+    /**
+     * Get the specified Connection.
+     *
+     * @return array|null The Connection or null if it doesn't exist.
+     */
+    public function getConnection($name);
+
+    /**
+     * Set the values of the specified Connection.
+     *
+     * @param array $connection The Connection described as an array.
+     *
+     * @return Backend\Base\Utilities\BindingFactory
+     */
+    public function setConnection($name, array $connection);
 }
